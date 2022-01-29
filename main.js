0f8b9976a16bbf5b0b884f20727556f17c308b0f
Jonathan Ventura <jonathan.venturarj@gmail.com>
2022-01-29 12:10:48 -0300

    Ajustes

--- main.js
+++ main.js
@@ -1,22 +1,20 @@
-$(document).ready(function(){     
-  
-    $(".total").blur(function(){
-        
-        teste()
-        valorTotal ()
-        
-    })
-    limparGeral()
-})
+$(document).ready(function () {
+  $(".total").blur(function () {
+    calculo();
+    valorTotal();
+  });
+  limparGeral();
+});
 
-$(function (){
-    var divContainer = $('#despesa');
-    var botaoAdd = $('#BotaoAdd');
-  
-    var i = 0;
+//Função responsavel por adicionar mais despesas
+$(function () {
+  var divContainer = $("#despesa");
+  var botaoAdd = $("#BotaoAdd");
 
-    $(botaoAdd).click(function(){
-        $(` <div id="despesa${i} " class="conteudoIndividual"> 
+  var i = 0;
+
+  $(botaoAdd).click(function () {
+    $(` <div id="despesa${i} " class="conteudoIndividual"> 
                 <div class="despesas linha" > 
                     <button type="button" class="remove" ><i class="fas fa-times"></i></button>
             
@@ -28,96 +26,93 @@ $(function (){
                         <option value="">Outra</option>
                     </select>
                    <input type="text" name="">
-                    <input type="text" name="quantidade"  class="quantidade total" onblur="teste()"/>
-                    <input type="text" name="valor_unitario"  class="valor_unitario total" onblur="teste()"/>
+                    <input type="text" name="quantidade"  class="quantidade total" onblur="calculo()"/>
+                    <input type="text" name="valor_unitario"  class="valor_unitario total" onblur="calculo()"/>
                     <input type="text" name="valor_total" class="valorTotal" disabled  />
                 </div>
-            </div>`).appendTo(divContainer)
-            i++;
-    });
+            </div>`).appendTo(divContainer);
+    i++;
+  });
 
-    $('#despesa').on('click', '.remove', function(){
-        
-        $(this).parents('.conteudoIndividual').remove();
-        i--;
-      
-        valorTotal ()
-       
-    })
+  //Remove as despesas adicionadas
+  $("#despesa").on("click", ".remove", function () {
+    $(this).parents(".conteudoIndividual").remove();
+    i--;
 
+    valorTotal();
+  });
 });
 
+// Função responsavel por fazer o subtotal
+function calculo() {
+  const totalGeral = [];
 
-function teste(){
+  $(".linha").each(function () {
+    // pega os valores dos inputs
+    var qtd = $(this).find("input[name=quantidade]").val();
+    var val = $(this).find("input[name=valor_unitario]").val();
 
-   const totalGeral = []
-   
-    $('.linha').each(function (){
-        
-        // pega os valores dos inputs 
-        var qtd = $(this).find('input[name=quantidade]').val();
-        var val = $(this).find('input[name=valor_unitario]').val();
+    var total = parseFloat(qtd) * parseFloat(val);
 
-       
-        var total = parseFloat(qtd) * parseFloat(val);
-
-        //tratamento do NaN
-        if(isNaN(total)){
-            $(this).find('input[name=valor_total]').val(" ").html(" ")
-        }else{
-            $(this).find('input[name=valor_total]').val(total.toLocaleString("pt-BR", { style: "currency" , currency:"BRL"})).html(total)
-        }
-        
-    
-        totalGeral.push(total)
-        
-    });
-
-    // validação input    
-    $('.linha').each(function (){
-     
-
-        var total1 =  $(this).find('input[name=valor_total]').val().replace(/[^0-9,]*/g, '').replace(',', '.')
-        
-        if(total1 >= 200){
-            $(this).addClass( "selected" ).hasClass("linha");
-                    
-        }else{
-            $(this).removeClass( "selected" ).hasClass("linha");
-        }       
-    });
-  
-    valorTotal ()
-}
-
-function valorTotal (){
-    var s1 = 0
-    $('.linha').each(function (){
-
-    var teste = parseFloat( $(this).find('input[name=valor_total]').val().replace(/[^0-9,]*/g, '').replace(',', '.'))
-
-    if (typeof teste == "number"  && !isNaN(teste)){
-        
-        s1 += teste
-         $('.totalDespesas > p').html(s1.toLocaleString("pt-BR", { style: "currency" , currency:"BRL"}));
-    }else{
-        
+    //tratamento do NaN
+    if (isNaN(total)) {
+      $(this).find("input[name=valor_total]").val(" ").html(" ");
+    } else {
+      $(this)
+        .find("input[name=valor_total]")
+        .val(
+          total.toLocaleString("pt-BR", { style: "currency", currency: "BRL" })
+        )
+        .html(total);
     }
-    
-    
-    });
- 
-   
-    
+
+    totalGeral.push(total);
+  });
+
+  // validação input
+  $(".linha").each(function () {
+    var total1 = $(this)
+      .find("input[name=valor_total]")
+      .val()
+      .replace(/[^0-9,]*/g, "")
+      .replace(",", ".");
+
+    if (total1 >= 200) {
+      $(this).addClass("selected").hasClass("linha");
+    } else {
+      $(this).removeClass("selected").hasClass("linha");
+    }
+  });
+
+  valorTotal();
 }
-function limparGeral(){
-    $(".limpar").on("click" ,function(){
-        $('.valorTotal').val("R$ 0.000,00").html("R$ 0.000,00")
-    })
 
+//Função responsavel por colocar o valor total Geral
+function valorTotal() {
+  var s1 = 0;
+  $(".linha").each(function () {
+    var teste = parseFloat(
+      $(this)
+        .find("input[name=valor_total]")
+        .val()
+        .replace(/[^0-9,]*/g, "")
+        .replace(",", ".")
+    );
+
+    if (typeof teste == "number" && !isNaN(teste)) {
+      s1 += teste;
+      $(".totalDespesas > p").html(
+        s1.toLocaleString("pt-BR", { style: "currency", currency: "BRL" })
+      );
+    } else {
+    }
+  });
 }
 
-
-
-
-
+// Função responsavel no botão Limpar
+function limparGeral() {
+  $(".limpar").on("click", function () {
+    $(".valorTotal").val("R$ 0.000,00").html("R$ 0.000,00");
+    $(".linha").removeClass("selected").hasClass("linha");
+  });
+}
